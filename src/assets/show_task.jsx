fb6d7013943bfd07d3f445eb61522cfb6a02dbad
FranzFQ <quemejavi@gmail.com>
2025-08-26 19:03:56 -0600

    feat(assets): deleting files search_task.jsx & order_task.jsx and changes in the logic for display tasks

--- src/assets/show_task.jsx
+++ src/assets/show_task.jsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
+import Order_Task from "./order_task";
 
 export default function Show_Task() {
-  const [task_filter, setTask_filter] = useState("all task");
+  const [task_filter, settask_filter] = useState("all task");
   const [tasks, settasks] = useState([]);
-  
+
   useEffect(() => {
-      const storedTasks = localStorage.getItem("tasks");
-      if (storedTasks) {
-        settasks(JSON.parse(storedTasks));
+    const storedTasks = localStorage.getItem("tasks");
+    if (storedTasks) {
+      settasks(JSON.parse(storedTasks));
     }
   }, []);
 
@@ -15,20 +16,56 @@ export default function Show_Task() {
     <div>
       <div>
         <h1>Tasks</h1>
-        <select name="task_filter" id="task_filter" onChange={(e) => setTask_filter(e.target.value)}>
+        <select
+          name="task_filter"
+          id="task_filter"
+          onChange={(e) => settask_filter(e.target.value)}
+        >
           <option value="all task">all task</option>
           <option value="to do">to do</option>
           <option value="done">done</option>
         </select>
       </div>
-      {tasks.map((task) => (
-        <div key={task.id} className="task">
-          <h2>{task.name}</h2>
-          <p>{task.state}</p>
-          {task.state === "to do" && <button>Complete</button>}
-          <button>Delete</button>
+      {task_filter === "all task" && (
+        <div>
+          <h2>All Tasks</h2>
+          {tasks.map((task) => (
+            <div key={task.id} className="task">
+              <h2>{task.name}</h2>
+              <p>{task.state}</p>
+              {task.state === "to do" && <button>Complete</button>}
+            </div>
+          ))}
         </div>
-      ))}
+      )}
+      {task_filter === "to do" && (
+        <div>
+          <h2>To Do Tasks</h2>
+          {tasks
+            .filter((task) => task.state === "to do")
+            .map((task) => (
+              <div key={task.id} className="task">
+                <h2>{task.name}</h2>
+                <p>{task.state}</p>
+                {task.state === "to do" && <button>Complete</button>}
+              </div>
+            ))}
+        </div>
+      )}
+      {task_filter === "done" && (
+        <div>
+          <h2>Done Tasks</h2>
+          {tasks
+            .filter((task) => task.state === "done")
+            .map((task) => (
+              <div key={task.id} className="task">
+                <h2>{task.name}</h2>
+                <p>{task.state}</p>
+                {task.state === "to do" && <button>Complete</button>}
+              </div>
+            ))}
+        </div>
+      )}
     </div>
   );
 }
