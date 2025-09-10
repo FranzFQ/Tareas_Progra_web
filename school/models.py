bd1181a58a7e0246bddbbed7c25e73f88254223b
FranzFQ <Quemejavi@gmail.com>
2025-09-10 12:54:07 -0600

    feat(school): create the connection between phone number to studen and teacher

--- school/models.py
+++ school/models.py
@@ -7,7 +7,7 @@ class Student(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     enrollment_date = models.DateField()
-    phone_number = models.CharField(max_length=15)
+    phone_number = models.ForeignKey("PhoneNumber", on_delete=models.SET_NULL, null=True, blank=True)
     email = models.EmailField(unique=True)
     emergency_number = models.CharField(max_length=15)
 
@@ -16,7 +16,7 @@ class Teacher(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     hire_date = models.DateField()
-    phone_number = models.CharField(max_length=15)
+    phone_number = models.ForeignKey("PhoneNumber", on_delete=models.SET_NULL, null=True, blank=True)
     email = models.EmailField(unique=True)
     course = models.CharField(max_length=50, default="General")
 
